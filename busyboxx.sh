503a0ee37c469731e5be4d317aa6b97a5cf3fb92
cbot918 <cbot918@gmail.com>
2023-07-31 20:24:18 +0800

    update

--- busyboxx.sh
+++ busyboxx.sh
@@ -61,4 +61,3 @@ cd initrd
 
 cd ..
 
-qemu-system-x86_64 -kernel bzImage -initrd initrd.img -nographic -append 'console=ttyS0'
--- pre-install.sh
+++ pre-install.sh
@@ -5,7 +5,7 @@
   sudo apt install libssl-dev libelf-dev libncurses5-dev libncursesw5-dev musl-tools make -y
 
   # install qemu
-  sudo apt install libpixman-1-dev libcairo2-dev libpango1.0-dev libjpeg8-dev libgif-dev ninja-build qemu -y
+  sudo apt install libpixman-1-dev libcairo2-dev libpango1.0-dev libjpeg8-dev libgif-dev ninja-build qemu qemu-system-x86-64 -y
 
   sudo ln -s /usr/include/linux /usr/include/x86_64-linux-musl/
   sudo ln -s /usr/include/asm-generic /usr/include/x86_64-linux-musl/
