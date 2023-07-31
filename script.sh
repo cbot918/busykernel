83cbf0a60a5ce9953f8fd5efb94b56e59d206690
cbot918 <cbot918@gmail.com>
2023-07-31 20:19:09 +0800

    update

--- script.sh
+++ script.sh
@@ -1,7 +1,11 @@
- 
-  sudo apt install libssl1.1=1.1.1f-1ubuntu2 -y
-  sudo apt install libssl-dev libelf-dev libncurses5-dev libncursesw5-dev musl-tools -y
 
+  sudo apt update
+  # install headers anad musl-gcc 
+  sudo apt install libssl1.1=1.1.1f-1ubuntu2
+  sudo apt install libssl-dev libelf-dev libncurses5-dev libncursesw5-dev musl-tools make -y
+
+  # install qemu
+  sudo apt install libpixman-1-dev libcairo2-dev libpango1.0-dev libjpeg8-dev libgif-dev ninja-build qemu -y
 
   sudo ln -s /usr/include/linux /usr/include/x86_64-linux-musl/
   sudo ln -s /usr/include/asm-generic /usr/include/x86_64-linux-musl/
